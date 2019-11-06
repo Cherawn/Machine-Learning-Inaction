30cda7a264944dc00343dfcfee94e9221d2d638a
Jason <33150579+Cherawn@users.noreply.github.com>
2019-11-06 09:28:24 +0800

    Update svmMLiA.py
    计算结果待验证。

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -16,7 +16,7 @@ def loadDataSet(fileName):
     with open(fileName) as fr:  # 逐行读取，滤除空格等
         for line in fr.readlines():
             lineArr = line.strip().split('\t')
-            dataMat.append(float(lineArr[0]), float(lineArr[1]))  # 添加数据
+            dataMat.append([float(lineArr[0]), float(lineArr[1])])  # 添加数据
             labelMat.append(int(lineArr[2]))  # 添加标签
     return dataMat, labelMat
 
@@ -75,28 +75,63 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     labelMat = np.mat(classLabels).transpose()
     b = 0
     m, n = np.shape(dataMatrix)
-    alphas = np.mat(np.zeros(m, 1))
+    alphas = np.mat(np.zeros((m, 1)))
     iter_num = 0
     while iter_num < maxIter:
         alphaPairsChanged = 0
         for i in range(m):
             fXi = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T) + b)
             Ei = fXi - float(labelMat[i])
-            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or \
-                    ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
                 j = selectJrand(i, m)
                 fXj = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T) + b)
                 Ej = fXj - float(labelMat[j])
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
-                if labelMat[j] != labelMat[j]:
-                    L = max(0, alphas[j] - alphas[i])
+                if labelMat[i] != labelMat[j]:
+                    L = max(0.0, alphas[j] - alphas[i])
                     H = min(C, C + alphas[j] - alphas[i])
                 else:
-                    L = max(0, alphas[j] + alphas[i] - C)
+                    L = max(0.0, alphas[j] + alphas[i] - C)
                     H = min(C, alphas[j] + alphas[i])
-                if L == H: print('L = H');continue
-                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
-                      dataMatrix[i, :] * dataMatrix[i, :].T - \
-                      dataMatrix[j, :] * dataMatrix[j, :].T
-                if eta > 0: print('eta > 0');continue
+                if L == H:
+                    print('L = H')
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - dataMatrix[i, :] * \
+                      dataMatrix[i, :].T - dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta > 0:
+                    print('eta > 0')
+                    continue
+                alphas[j] += labelMat[i] * (Ei - Ej) / eta
+                alphas[j] = clipAlpua(alphas[j], H, L)
+                if abs(alphas[j] - alphaJold) < 0.00001:
+                    print('j not moving enough')
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * \
+                     dataMatrix[i, :] * dataMatrix[i, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJold) * \
+                     dataMatrix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * \
+                     dataMatrix[i, :] * dataMatrix[j, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJold) * \
+                     dataMatrix[j, :] * dataMatrix[j, :].T
+                if (0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaPairsChanged += 1
+                print("iter: %d i: %d, pairs changed %d" % (iter_num, i, alphaPairsChanged))
+        if alphaPairsChanged == 0:
+            iter_num += 1
+        else:
+            iter_num = 0
+        print("interation number: %d" % iter_num)
+    return b, alphas
+
+
+if __name__ == '__main__':
+    dataArr, labelArr = loadDataSet('testSet.txt')
+    b_out, alphas_out = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
