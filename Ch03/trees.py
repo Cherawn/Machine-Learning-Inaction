c3ec873101c2fb18eb63e00915bbe89ee24335e1
Jason <33150579+Cherawn@users.noreply.github.com>
2019-11-18 17:27:42 +0800

    Update trees.py

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -67,8 +67,28 @@ def majorityCnt(classList):
     return sortedClassCount[0][0]
 
 
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree\
+            (splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
 if __name__ == '__main__':
     myData, labels = creatDataSet()
     # retDataSet = splitDataSet(myData, 0, 1)
     # shannonEnt = calcShannonEnt(myData)
-    feature = chooseBestFeatureToSplit(myData)
+    # feature = chooseBestFeatureToSplit(myData)
+    myTree = createTree(myData, labels)
